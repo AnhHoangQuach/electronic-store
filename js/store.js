2f66ee945c65377a0832870930a2803831879522
hoanganh <hoanganh@AHQ.dev>
2019-09-24 06:22:14 +0700

    merge later

--- js/store.js
+++
@@ -1,8 +0,0 @@
-$('input.price').keyup(function(event) {
-  	// skip for arrow keys
-  	if(event.which >= 37 && event.which <= 40) return;
-  	// format number
-  		$(this).val(function(index, value) {
-    	return value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-  	});
-});
